3df3939601d876f32f81cdd09b0f2a28ad6782ff
admin <admin@DESKTOP-6JOPKGM>
2025-07-31 18:11:28 +0530

    first commit

---
+++ OPERATORS/src/methods/java/Abstract_method.java
@@ -0,0 +1,58 @@
+package methods.java;
+
+/*abstract class Abstract_method {
+	abstract void
+	check(String n);
+}
+
+public class Geeks extends Abstract_method {            //not get output
+	public static void main(String[] args)
+	{
+	   Geeks ob = new Geeks();
+	   
+	   ob.check("Geeksforgeeks");
+	}
+	
+	@Override void
+	check(String n)
+	{
+		System.out.println(n);
+	}
+	
+
+}
+*/
+
+
+/*import java.io.*;                                //not get output
+class Abstract_method{
+	static void hello()
+	{
+		System.out.println("Hello");
+	}
+}
+
+class Main  {
+	public static void main(String[] args) {
+		Abstract_method.hello();
+	}
+}       */
+
+
+
+
+/*import java.io.*;
+class Abstract_method{
+	public static void greet() {
+		System.out.println("hello world");
+	}
+	public static void main(String[] args) {
+		greet();
+	}
+} */
+
+
+
+
+
+
