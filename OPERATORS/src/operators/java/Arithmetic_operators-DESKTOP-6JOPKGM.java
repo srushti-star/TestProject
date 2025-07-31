d9fd101e031a25d45fce4ec85c3fd7f5d4f5296a
admin <admin@DESKTOP-6JOPKGM>
2025-07-31 18:23:28 +0530

    whole project

---
+++ OPERATORS/src/operators/java/Arithmetic_operators-DESKTOP-6JOPKGM.java
@@ -0,0 +1,62 @@
+package operators.java;
+
+public class Arithmetic_operators {
+       public static void main(String[] args) 
+       {
+    	   int a = 10;
+    	   int b = 20;
+    	   
+    	   System.out.println("a + b = " + (a+b));
+    	   System.out.println("a - b = " + (a - b));
+    	   System.out.println("a * b = " + (a * b));
+    	   System.out.println("a / b = " + (a / b));
+    	   System.out.println("a % b = " + (a % b));
+    	   
+    	   
+       
+       
+       
+       int num1 = 10; int num2 = 20; int sum2= 0;                     //second type of write code
+       
+       System.out.println("num1 = " + num1);
+       System.out.println("num2 = " + num2);
+       
+       sum2 = num1 + num2;
+       
+       System.out.println("The sum = " + sum2);
+       
+       
+       
+       
+    
+       int num_1 = 10;                                              //third type of write code
+       int num_2 = 20;
+       
+       int mul = num_1 * num_2;
+       
+       System.out.println("The multiplication = " + mul);
+       
+      
+       
+       
+       
+       
+       String str1 = "HELLO";                                   //string addition
+       String str2 = "WORLD";
+       
+       String result = str1 + " " + str2;
+       
+       System.out.println(result);
+       
+       
+       
+       int  k , l , m ;
+       k = 100;
+       l = 200;
+       m = k + l;
+      
+       System.out.println(m);
+
+       }
+
+}
